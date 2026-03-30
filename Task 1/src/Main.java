8059b124461a1e9beafa43a603d0d427755f4cbd
lovelymiadori <alisalis641@gmail.com>
2026-03-30 07:00:26 +0300

    дзшка

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
@@ -5,14 +7,37 @@ public class Main {
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
-        //todo реализовать ввод пользователем кол-ва ингредиентов.
+        Scanner scanner = new Scanner(System.in);
 
-        //Рецепты Эликсиров:
-        // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
-        // 2. 2 кости ворона + 4 пельменя - Эликсир стойкости
-        // 3. 7 слез вурдалака + 1 пельмень + 2 жабьих глаза - Эликсир скрытности
-        // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
+        System.out.println("Введите количество жабьих глаз:");
+        toadEyesCount = scanner.nextInt();
+        System.out.println("Введите количество слез вурдалака:");
+        ghoulTearsCount = scanner.nextInt();
+        System.out.println("Введите количество костей ворона:");
+        ravenBonesCount = scanner.nextInt();
+        System.out.println("Введите количество пельменей:");
+        dumplingsCount = scanner.nextInt();
 
-        //todo дописать логику программы сюда
+        boolean isCanMake = false;
+
+        if (toadEyesCount >= 3 && ravenBonesCount >= 1) {
+            System.out.println("Вы можете изготовить элексир зоркости");
+            isCanMake = true;
+        }
+        if (ravenBonesCount >= 2 && dumplingsCount >= 4) {
+            System.out.println("Вы можете изготовить элексир стойкости");
+            isCanMake = true;
+        }
+        if (ghoulTearsCount >= 7 && dumplingsCount >= 1 && toadEyesCount >= 2) {
+            System.out.println("Вы можете изготовить элексир скрытности");
+            isCanMake = true;
+        }
+        if (ghoulTearsCount >= 5 && dumplingsCount >= 10 && toadEyesCount >= 4 && ravenBonesCount >= 3) {
+            System.out.println("Вы можете изготовить запретный элексир");
+            isCanMake = true;
+        }
+        if (!isCanMake) {
+            System.out.println("Вы не можете изготовить ни одного элексира");
+        }
     }
 }
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        for (int i = 1; i <= 100; i ++) {
+            if (i % 2 == 0) {
+                System.out.println("Уволен айтишник с айди " + i);
+            }
+        }
+
+        /* или
+        for (int i = 2; i <= 100; i += 2) {
+            System.out.println("Уволен айтишник с айди " + i);
+        }
+         */
+
+        int i = 2;
+        while (i % 2 == 0) {
+            System.out.println("Уволен айтишник с айди " + i);
+            i += 2;
+            if (i >= 101) {
+                break;
+            }
+        }
+
+        /* или
+        while (i <= 100) {
+    System.out.println("Уволен айтишник с айди " + i);
+    i += 2;
+        }
+         */
     }
 }
